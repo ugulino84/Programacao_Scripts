b3d12c03a4060ca25087131f0b2818a4d7f72642
Marcos Ugulino <marcos.ugulino@academico.ifpb.edu.br>
2021-03-24 09:33:39 -0300

    Correção do Ex.03 da Lista 05

--- Semana 06/ex_03.sh
+++ Semana 06/ex_03.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -e "Escreva um script que peça para o usuário digitar 2 nomes de arquivo e imprima o nome daquele que possui o maior número de linhas.\n"
 
-
-(( $(wc -l < $1) > $(wc -l < $2) )) && echo "$1" || echo "$2" 
+read -p "Digite o nome do primeiro arquivo: " a
+read -p "Digite o nome do segundo arquivo: " b
+(( $(wc -l < "${a}") > $(wc -l < "${b}") )) && echo "${a}" || echo "${b}" 
