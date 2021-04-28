f3d18839d2f6c759739d850ccb1fe9c23d5c2db3
Marcos Ugulino <marcos.ugulino@academico.ifpb.edu.br>
2021-04-28 13:48:44 -0300

    Ex04.sh atualizado.

--- Semana 11/ex04.sh
+++ Semana 11/ex04.sh
@@ -12,14 +12,27 @@ read -p "Digite um endereço ip: " ip
 
 echo -e "${ip}" > /tmp/ip.txt
 
-#separando os octetos utilizando o comando cut
+#separando os octetos utilizando o comando cut e criando os correspondentes binários
 
 octeto1=$(cat /tmp/ip.txt | cut -d '.' -f 1)
-octeto2=$(cat /tmp/ip.txt | cut -d '.' -f 2)
-octeto3=$(cat /tmp/ip.txt | cut -d '.' -f 3)
-octeto4=$(cat /tmp/ip.txt | cut -d '.' -f 4)
+ob1=$(echo -e "obase=2; ${octeto1}" | bc)
+while [ $(echo -e "$ob1" | wc -c) -lt 9 ]; do ob1=$(sed 's/^/0/' <<< "$ob1"); done
 
-echo -e "Octeto #1: ${octeto1} em binário é $(echo "obase=2; ${octeto1}" | bc)"
-echo -e "Octeto #2: ${octeto2} em binário é $(echo "obase=2; ${octeto2}" | bc)"
-echo -e "Octeto #3: ${octeto3} em binário é $(echo "obase=2; ${octeto3}" | bc)"
-echo -e "Octeto #4: ${octeto4} em binário é $(echo "obase=2; ${octeto4}" | bc)"
+octeto2=$(cat /tmp/ip.txt | cut -d '.' -f 2)
+ob2=$(echo -e "obase=2; ${octeto2}" | bc)
+while [ $(echo -e "$ob2" | wc -c) -lt 9 ]; do ob2=$(sed 's/^/0/' <<< "$ob2"); done
+
+octeto3=$(cat /tmp/ip.txt | cut -d '.' -f 3)
+ob3=$(echo -e "obase=2; ${octeto3}" | bc)
+while [ $(echo -e "$ob3" | wc -c) -lt 9 ]; do ob3=$(sed 's/^/0/' <<< "$ob3"); done
+
+octeto4=$(cat /tmp/ip.txt | cut -d '.' -f 4)
+ob4=$(echo -e "obase=2; ${octeto4}" | bc)
+while [ $(echo -e "$ob4" | wc -c) -lt 9 ]; do ob4=$(sed 's/^/0/' <<< "$ob4"); done
+
+#imprimindo o resultado na tela
+
+echo -e "Octeto #1: ${octeto1} em binário é ${ob1}"
+echo -e "Octeto #2: ${octeto2} em binário é ${ob2}"
+echo -e "Octeto #3: ${octeto3} em binário é ${ob3}"
+echo -e "Octeto #4: ${octeto4} em binário é ${ob4}"
