d9afd534655b2741e03e96d802c97274a55240da
Marcos Antônio Ugulino de Araújo Filho <60929819+ugulino84@users.noreply.github.com>
2021-05-11 13:20:53 -0300

    Atividades da Lista 11 - Sed

--- Semana 13/ex_03.sh
+++ Semana 13/ex_03.sh
@@ -2,4 +2,4 @@
 
 #3 - Escreva um script que remova todos os números de CPF de um arquivo de entrada, alterando o seu valor para **CENSURADO**.
 
-sed -i -E 's/[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}/**CENSURADO**/g' $1
+sed -i -E 's/[0-9]{3}\.[0-9]{3}\.[0-9]{3}-[0-9]{2}/**CENSURADO**/g' $1
